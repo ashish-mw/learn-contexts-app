709962c10313dadb61aa24e64938bf6e39ffa93a
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2023-10-13 18:07:47 +0530

    add theme context

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const IndexPage = () => {
+  return (
+    <Layout title="My context app">
+      <h2>Index page</h2>
+    </Layout>
+  );
+};
+
+export default IndexPage;
