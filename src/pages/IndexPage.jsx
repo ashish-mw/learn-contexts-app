299ea2c3b7b0d9f949f592870a90883ff518e21c
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2023-10-13 18:12:51 +0530

    add one more example

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
+
 import Layout from "../components/Layout";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const IndexPage = () => {
+  const themeContext = useContext(ThemeContext);
+
   return (
     <Layout title="My context app">
       <h2>Index page</h2>
+
+      <p>Current theme: {themeContext.value}</p>
     </Layout>
   );
 };
